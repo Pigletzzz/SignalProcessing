eecaccd33c5cfafc8c54cf6dec5305c5b81cc11f
pun-zeoncoeng <90940019+Pigletzzz@users.noreply.github.com>
2024-02-29 19:09:29 +0800

    Feat: init env

---
+++ main.py
@@ -0,0 +1,15 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow,QMessageBox
+from mainwindow import *
+
+class MyWindow(QMainWindow,Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(MyWindow, self).__init__(parent)
+        self.setupUi(self)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    myWin = MyWindow()
+    myWin.show()
+    sys.exit(app.exec_())
