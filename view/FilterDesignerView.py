9abacffb7b8f34ea157bedc5da2254de30f56466
pun-zeoncoeng <90940019+Pigletzzz@users.noreply.github.com>
2024-03-22 15:55:15 +0800

    Refactor: 使用单词"audio"代替"voice"

--- view/FilterDesignerView.py
+++ view/FilterDesignerView.py
@@ -1,6 +1,5 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QWidget, QStackedWidget, QLabel
-
 from qfluentwidgets import Pivot
 
 from ui.Ui_FilterDesignerInterface import Ui_FilterDesignerInterface
