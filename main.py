4deabce168bc0299c2ad36e4b68da7907fb64f8c
pun-zeoncoeng <90940019+Pigletzzz@users.noreply.github.com>
2024-03-24 15:57:05 +0800

    Feat: 滤波器设计界面的两列interface

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ from PyQt5.QtWidgets import QApplication
 
 from controller.AudioFileController import AudioFileController
 from controller.route_controller import RouteController
+from convert2py import convertUi2Py
 from model.AudioModel import AudioModel
 from view.MainWindowView import MainWindow
 
 if __name__ == '__main__':
     try:
+        # TEST 执行一次convert编译ui文件
+        convertUi2Py()
+
         app = QApplication(sys.argv)
         myWin = MainWindow()
 
